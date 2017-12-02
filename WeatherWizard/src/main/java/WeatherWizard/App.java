eef692fcc23f0999761868cfba1caa7d41f066a9
Yann Colina <yanncolina@gmail.com>
2017-12-02 12:53:11 +0100

    [OpenWeatherMap] Adding request/response pattern

--- WeatherWizard/src/main/java/WeatherWizard/App.java
+++ WeatherWizard/src/main/java/WeatherWizard/App.java
@@ -1,5 +1,11 @@
 package WeatherWizard;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String openWeatherMapKey = "7f484592e9396fae071a8f19acdc0e71";
+        ActorSystem system = ActorSystem.create("WeatherWizard");
+        Http httpGate = Http.get(system);
+        Materializer materializer = ActorMaterializer.create(system);
+        ActorRef openWeatherMap = system.actorOf(OpenWeatherMap.props(httpGate, materializer));
+
+        openWeatherMap.tell(new OpenWeatherMapRequest(openWeatherMapKey), ActorRef.noSender());
+
+        // system.terminate();
     }
 }
