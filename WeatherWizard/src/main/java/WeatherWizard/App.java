fbc1642a6c77ff55feb408c10c8fbc32eedf7a4d
Yann Colina <yanncolina@gmail.com>
2017-12-02 18:57:15 +0100

    structure

--- WeatherWizard/src/main/java/WeatherWizard/App.java
+++ WeatherWizard/src/main/java/WeatherWizard/App.java
@@ -1,5 +1,7 @@
 package WeatherWizard;
 
+import WeatherWizard.OpenWeatherMap.ApiActor;
+import WeatherWizard.OpenWeatherMap.CurrentWeatherRequest;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
@@ -10,17 +12,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        String openWeatherMapKey = "7f484592e9396fae071a8f19acdc0e71";
         ActorSystem system = ActorSystem.create("WeatherWizard");
 
         // httpGate and materializer are needed to send and receive http requests
         Http httpGate = Http.get(system);
         Materializer materializer = ActorMaterializer.create(system);
 
-        ActorRef openWeatherMap = system.actorOf(OpenWeatherMap.props(httpGate, materializer), OpenWeatherMap.name);
+        ActorRef openWeatherMapApiRef = system.actorOf(ApiActor.props(httpGate, materializer), ApiActor.name);
 
-        openWeatherMap.tell(new OpenWeatherMapCurrentWeatherRequest(openWeatherMapKey, "Roma", "it"),
-                            ActorRef.noSender());
+        openWeatherMapApiRef.tell(new CurrentWeatherRequest("Roma", "it"),
+                                  ActorRef.noSender());
 
         // system.terminate();
     }
