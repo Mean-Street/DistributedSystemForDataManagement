83545be5b07181fc08d28998acd2465ac84f46d2
PiggyGenius <ludo.itrts@gmail.com>
2017-12-06 22:48:30 +0000

    On the way

--- kafka/kafka_docker/run_docker.sh
+++ kafka/kafka_docker/run_docker.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-zookeeper_address=192.168.1.72
-zookeeper_port=2182
+#zookeeper_address=192.168.1.72
+#zookeeper_port=2182
 
-docker pull wurstmeister/kafka
+#docker pull wurstmeister/kafka
+#dockerd
+#docker-compose -f docker-compose-single-broker.yml up
+#./kafka-topics.sh --create --zookeeper $zookeeper_address:$zookeeper_port --replication-factor 1 --partitions 10 --topic meteodata
 dockerd
-docker-compose -f docker-compose-single-broker.yml up
-./kafka-topics.sh --create --zookeeper $zookeeper_address:$zookeeper_port --replication-factor 1 --partitions 10 --topic meteodata
+docker pull zookeeper
+git clone https://github.com/wurstmeister/kafka-docker build kafka-docker/ -t kafka_docker
+docker run --name smack-zookeeper -p 2181:2181 -d zookeeper
