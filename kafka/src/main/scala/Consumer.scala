033687fd1ff4fa7cacbe59d11659f2684968b3c6
Ubuntu <ubuntu@ip-172-31-16-94.us-west-2.compute.internal>
2017-12-07 21:53:47 +0000

    Add basic samples of producer and consumer

--- kafka/src/main/scala/Consumer.scala
+++ kafka/src/main/scala/Consumer.scala
@@ -1,3 +1,5 @@
+// https://github.com/smallnest/kafka-example-in-scala
+
 import java.util.concurrent._
 import java.util.{Collections, Properties}
 
@@ -8,24 +10,31 @@ import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
 import scala.collection.JavaConversions._
 
 object Consumer extends App {
-  /*
-  val topic = args(1)
-  val brokers = args(2)
+  val Array(topic, groupId, brokers) = args
 
-  Properties props = new Properties();
-  props.put("bootstrap.servers", brokers);
-  props.put("group.id", "group-test");
-  //props.put("enable.auto.commit", "true");
-  //props.put("auto.commit.interval.ms", "1000");
-  props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+  val props = new Properties()
+  props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
+  props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId)
+  props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "10")
+  props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
+  props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000")
+  props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000")
+  props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
 
   val consumer = new KafkaConsumer[String, String](props)
-  consumer.subscribe(Arrays.asList("foo", "bar"));
-  while (true) {
-    ConsumerRecords<String, String> records = consumer.poll(100);
-    for (ConsumerRecord<String, String> record : records)
-      println("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value())
+  consumer.subscribe(Collections.singletonList(topic))
+
+  for(i <- 1 to 10) {
+    val timeout = 1000
+    println("Polling...")
+    val records = consumer.poll(timeout)
+    println("Polled", records.count(), "records")
+    for (record <- records) {
+      System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset())
+    }
+    Thread.sleep(1000)
   }
-  */
+
+  consumer.close()
 }
