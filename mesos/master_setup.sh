ab0aeeae06ef030c62a83cb3cd7a48bafe914bae
Nicolas Meyer <nicolas.meyer@grenoble-inp.org>
2017-12-07 14:26:18 +0100

    Update scripts Mesos. Includes working Marathon.

--- mesos/master_setup.sh
+++ mesos/master_setup.sh
@@ -3,34 +3,33 @@
 MASTER_IP=10.0.0.100
 
 #Install mesosphere
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
-#DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
-#CODENAME=$(lsb_release -cs)
-#echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | \
-#sudo tee /etc/apt/sources.list.d/mesosphere.list
-#sudo apt-get -y update
-#sudo apt-get -y install mesos marathon
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
+echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | \
+sudo tee /etc/apt/sources.list.d/mesosphere.list
+sudo apt-get -y update
+sudo apt-get -y install mesos marathon
 
 #Configure master
-#sudo service mesos-slave stop
-#echo manual | sudo tee /etc/init/mesos-slave.override
-#echo ${MASTER_IP} | sudo tee /etc/mesos-master/MASTER_IP
-#echo zk://${MASTER_IP}:2181/mesos | sudo tee /etc/mesos/zk
-#echo SDTDCluster | sudo tee /etc/mesos-master/cluster
-#echo ${MASTER_IP} | sudo tee /etc/mesos-master/hostname
+sudo service mesos-slave stop
+echo manual | sudo tee /etc/init/mesos-slave.override
+echo ${MASTER_IP} | sudo tee /etc/mesos-master/ip
+echo zk://${MASTER_IP}:2181/mesos | sudo tee /etc/mesos/zk
+echo SDTDCluster | sudo tee /etc/mesos-master/cluster
+echo ${MASTER_IP} | sudo tee /etc/mesos-master/hostname
 
 #Configure zookeeper
-#echo 1 | sudo tee /etc/mesos-master/quorum
-#echo 1 | sudo tee /etc/zookeeper/conf/myid
+echo 1 | sudo tee /etc/mesos-master/quorum
+echo 1 | sudo tee /etc/zookeeper/conf/myid
 
 #Start
-#sudo service zookeeper restart
-#sudo service mesos-master restart
-#marathon --master zk://${MASTER_IP}:2181/mesos
-#sudo service marathon restart
+sudo service zookeeper restart
+sudo service mesos-master restart
+marathon --master zk://${MASTER_IP}:2181/mesos --zk zk://${MASTER_IP}:2181/marathon
 
 #Zookeeper
-sudo docker run -d --net=host netflixoss/exhibitor:1.5.2
+#sudo docker run -d --net=host netflixoss/exhibitor:1.5.2
 #Mesos master
 #sudo docker run -d --net=host \
 #  -e MESOS_PORT=5050 \
@@ -58,29 +57,7 @@ sudo docker run -d --net=host netflixoss/exhibitor:1.5.2
 #  -p 8080:8080 \
 #  mesosphere/marathon --master zk://10.0.0.100:2181/mesos --zk zk://10.0.0.100:2181/marathon
 
-#Install mesosphere
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
-DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
-CODENAME=$(lsb_release -cs)
-echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | \
-sudo tee /etc/apt/sources.list.d/mesosphere.list
-sudo apt-get -y update
-sudo apt-get -y install mesos marathon
 
-#Configure master
-sudo service mesos-slave stop
-echo manual | sudo tee /etc/init/mesos-slave.override
-echo ${MASTER_IP} | sudo tee /etc/mesos-master/ip
-echo zk://${MASTER_IP}:2181/mesos | sudo tee /etc/mesos/zk
-echo SDTDCluster | sudo tee /etc/mesos-master/cluster
-echo ${MASTER_IP} | sudo tee /etc/mesos-master/hostname
 
-#Configure zookeeper
-echo 1 | sudo tee /etc/mesos-master/quorum
-echo 1 | sudo tee /etc/zookeeper/conf/myid
-
-#Start
-sudo service zookeeper stop
-sudo service mesos-master restart
 
 
