38edb42b44054b228cba20a91b22270af2c94362
PiggyGenius <ludo.itrts@gmail.com>
2017-12-07 15:56:20 +0100

    Update create_topic, it doesn't work

--- kafka/create_topic.sh
+++ kafka/create_topic.sh
@@ -4,5 +4,4 @@ zooaddress=$1
 zooport=$2
 topic=$3
 
-git pull kafka-docker/
 kafka-docker/create-topics.sh --create --zookeeper $zooaddress:$zooport --replication-factor 1 --partitions 10 --topic $topic
