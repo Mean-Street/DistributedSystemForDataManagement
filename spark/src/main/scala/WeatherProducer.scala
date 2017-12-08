6bff073d7727ddbfb44e4619b5b48ed1ee520a3c
Clément Parizot <clement.parizot@grenoble-inp.org>
2017-12-08 10:28:18 +0100

    corrections dépendences

--- spark/src/main/scala/WeatherProducer.scala
+++ spark/src/main/scala/WeatherProducer.scala
@@ -3,12 +3,11 @@ import java.util.Properties
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import com.datastax.spark.connector._
 import org.apache.spark.sql.cassandra._
 
 //Usage WeatherProducer "topics" "@broker1,@broker2"
-    /*
 object WeatherProducer extends App {
-  def main(args: Array[String]) {
     val topic = args(0)
     val brokers = args(1)
     val props = new Properties()
@@ -26,14 +25,15 @@ object WeatherProducer extends App {
     //TODO
     val firstRow = sc.cassandraTable("sdtd", "temperatures").first()
 
-    val date : String = firstRow.get(0); //.select("date")
-    val temp : Double = firstRow.get(1);
+    val date : String = firstRow.getString("date");
+    val temp : Double = firstRow.getDouble("temperature");
+    
+    /*val date : String = "08/12/17"
+    val temp : Double = 3.2*/
 
     //Send to brokers
     val data = new ProducerRecord[String, Double](topic, date, temp)
     producer.send(data)
 
     producer.close()
-  }
 }
-    */
