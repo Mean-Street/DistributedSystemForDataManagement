5e4ad5f8d3e61a6cb4486fcf5de8a984c19aa11d
Ubuntu <ubuntu@ip-172-31-16-232.us-west-2.compute.internal>
2017-12-08 09:33:34 +0000

    merge

--- spark/src/main/scala/WeatherProducer.scala
+++ spark/src/main/scala/WeatherProducer.scala
@@ -3,12 +3,12 @@ import java.util.Properties
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import com.datastax.spark.connector._
 import org.apache.spark.sql.cassandra._
 
 //Usage WeatherProducer "topics" "@broker1,@broker2"
     /*
 object WeatherProducer extends App {
-  def main(args: Array[String]) {
     val topic = args(0)
     val brokers = args(1)
     val props = new Properties()
@@ -26,8 +26,10 @@ object WeatherProducer extends App {
     //TODO
     val firstRow = sc.cassandraTable("sdtd", "temperatures").first()
 
-    val date : String = firstRow.get(0); //.select("date")
-    val temp : Double = firstRow.get(1);
+    val date : String = "date"
+    val temp : Double = 5.2
+    //val date : String = firstRow.get(0); //.select("date")
+    //val temp : Double = firstRow.get(1);
 
     //Send to brokers
     val data = new ProducerRecord[String, Double](topic, date, temp)
