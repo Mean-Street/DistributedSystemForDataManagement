589060f341721efe298d733a30d5473934ba135d
Nicolas Meyer <nicolas.meyer@grenoble-inp.org>
2017-12-15 12:41:38 +0100

    Test deploiement akka avec marathon

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM maven
-COPY * /tmp/SDTD/WeatherFinder/
+ADD WeatherFinder /tmp/SDTD/WeatherFinder/
 WORKDIR /tmp/SDTD/WeatherFinder
 ENTRYPOINT /tmp/SDTD/WeatherFinder/docker_entrypoint.sh
--- WeatherFinder/docker_entrypoint.sh
+++ WeatherFinder/docker_entrypoint.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
+ls
 mvn compile
-mvn exec:java -Dexec.args="0 1 2"
+mvn exec:java -Dexec.args="10.0.0.200 1234 randomstring"
+
+
+#sudo docker build . -t test_akka
+#sudo docker run --attach STDOUT test_akka
