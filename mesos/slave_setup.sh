987a78cace216d54e9fc4b5a0251c24a8ff82b51
Nicolas Meyer <nicolas.meyer@grenoble-inp.org>
2018-01-02 15:29:27 +0100

    Corrections mineures scripts mesos

--- mesos/slave_setup.sh
+++ mesos/slave_setup.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-SALVE_IP=10.0.0.200
+SLAVE_IP=10.0.0.210
 MASTER_IP=10.0.0.100
 
 #Install mesosphere
@@ -12,15 +12,18 @@ sudo tee /etc/apt/sources.list.d/mesosphere.list
 sudo apt-get -y update
 sudo apt-get -y install mesos
 
+#install docker
+sudo apt-get -y install docker.io
+
 #Configure slave
 sudo service mesos-master stop
 sudo service zookeeper stop
 echo manual | sudo tee /etc/init/mesos-master.override
 echo manual | sudo tee /etc/init/zookeeper.override
 sudo apt-get -y remove --purge zookeeper
-echo ${SALVE_IP} | sudo tee /etc/mesos-slave/ip
+echo ${SLAVE_IP} | sudo tee /etc/mesos-slave/ip
 echo zk://${MASTER_IP}:2181/mesos | sudo tee /etc/mesos/zk
-echo ${SALVE_IP} | sudo tee /etc/mesos-slave/hostname
+echo ${SLAVE_IP} | sudo tee /etc/mesos-slave/hostname
 echo "cgroups/cpu,cgroups/mem" | sudo tee /etc/mesos-slave/isolation
 
 echo 'docker,mesos' | sudo tee /etc/mesos-slave/containerizers
@@ -28,4 +31,3 @@ echo '10mins' | sudo tee /etc/mesos-slave/executor_registration_timeout
 
 #Start
 sudo service mesos-slave restart
-
