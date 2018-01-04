50e5f4bddd80782ea7e7f25739ad08e8417a8a21
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-01-04 20:23:55 +0100

    Fix entrypoint

--- spark/docker-entrypoint.sh
+++ spark/docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 cd "$REPO/spark/app"
+
 if ! sbt assembly; then
   exit 1
 fi
+
+cp target/scala-2.11/SDTD-assembly-1.0.jar /sdtd.jar
+cp -r conf /conf
+
+# Execute command passed to docker run
+exec "$@"
