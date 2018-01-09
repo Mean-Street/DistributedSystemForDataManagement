8b3402e15be9e3e959440087f02fd7eece745169
Maxime Deloche <maxime.deloche@ensimag.grenoble-inp.fr>
2018-01-09 11:03:28 +0100

    All fixed

--- cassandra/launch_cassandra.sh
+++ cassandra/launch_cassandra.sh
@@ -1,18 +1,18 @@
 
 # 1. Launch Cassandra
-cassandra -R -f > /dev/null & 
+/usr/local/bin/docker-entrypoint.sh cassandra -f &
 
 # 2. Wait for Cassandra to be up
 while ! cqlsh -e "describe cluster" > /dev/null 2>&1; do
 	sleep 1
 done
 
-# 3. Create tables in the database
+# # 3. Create tables in the database
 cqlsh -f "$1"
 
-# 4. Signal Cassandra has been successfully launched
+# # 4. Signal Cassandra has been successfully launched
 echo "Cassandra successfully launched."
 
-# 5. Make sure script does not end (so that container keeps running)
+# # 5. Make sure script does not end (so that container keeps running)
 tail -f /dev/null
 
