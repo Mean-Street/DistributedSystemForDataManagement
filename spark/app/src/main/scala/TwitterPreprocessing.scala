5b11ef11f96eb5b116cd92325ca58bdd733d2bff
Maxime Deloche <maxime.deloche@ensimag.grenoble-inp.fr>
2018-01-10 21:51:41 +0100

    Add Sentiment Analysis in spark

--- spark/app/src/main/scala/TwitterPreprocessing.scala
+++ spark/app/src/main/scala/TwitterPreprocessing.scala
@@ -2,18 +2,31 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import com.datastax.spark.connector._
 import org.apache.spark.sql.cassandra._
+import utils.SentimentAnalysisUtils._
 
 
 object TwitterPreprocessing {
   def main(args: Array[String]) {
-    if (args.length < 2) {
-      System.exit(1)
-    }
 
     val sconf = new SparkConf()
     val sc = new SparkContext(sconf)
 
-    val rdd = sc.cassandraTable("sdtd", "tweet")
-    rdd.collect().foreach(println)
+    // TODO: load tweets from Kafka
+    val tweets = sc.parallelize(Array(
+    "",
+    "Mark is a really good football player",
+    "Mark is a good football player",
+    "Mark is an OK football player",
+    "Mark is a bad football player",
+    "Mark is a really bad football player",
+    "Pizzas are amazing",
+    "Amazing !",
+    "The grass is green",
+    "Horrible", 
+    "That TV show was perfect!"
+    ))
+
+    val sentiments = tweets.map(tweet => (tweet, detectSentiment(tweet)))
+    sentiments.foreach(println)
   }
 }
