dd6e4856b5a99e6fdc519b52f20d77889795ba16
Ubuntu <ubuntu@ip-10-0-0-200.us-west-2.compute.internal>
2018-01-10 11:09:41 +0000

    Weatherfinder config file modification to take environment variables

--- WeatherFinder/docker_entrypoint.sh
+++ WeatherFinder/docker_entrypoint.sh
@@ -2,4 +2,4 @@
 
 #===--- Stazrt script for the docker container ---===
 
-mvn exec:java -Dexec.args="${KAFKA_IP} ${KAFKA_PORT} ${KAFKA_TOPICS}"
+mvn exec:java -Dexec.args="${KAFKA_IP} ${KAFKA_PORT} ${KAFKA_TOPIC}"
