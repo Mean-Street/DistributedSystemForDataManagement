fc055a4b4e8f3b014331243077104a3d32818ba0
PiggyGenius <ludo.itrts@gmail.com>
2018-01-10 18:47:16 +0000

    Update code

--- spark/app/src/main/scala/TwitterPreprocessing.scala
+++ spark/app/src/main/scala/TwitterPreprocessing.scala
@@ -1,26 +1,3 @@
-//import org.apache.spark.SparkContext
-//import org.apache.spark.SparkConf
-//import com.datastax.spark.connector._
-//import org.apache.spark.sql.cassandra._
-
-
-//object TwitterPreprocessing {
-  //def main(args: Array[String]) {
-    ////if (args.length < 2) {
-      ////System.exit(1)
-    ////}
-
-    //val sconf = new SparkConf()
-    //val sc = new SparkContext(sconf)
-
-    //val rdd = sc.cassandraTable("sdtd", "tweet")
-    //println("Start print")
-    //rdd.collect().foreach(println)
-  //}
-//}
-
-
-
 import kafka.serializer.StringDecoder
 
 import org.apache.spark.SparkContext
@@ -38,6 +15,7 @@ import com.datastax.spark.connector.streaming._
 object TwitterPreprocessing {
   def main(args: Array[String]) {
     if (args.length < 5) {
+      println("Not enough arguments given.")
       System.exit(1)
     }
 
@@ -52,31 +30,19 @@ object TwitterPreprocessing {
     val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
       ssc, kafkaParams, topicsSet)
 
-    /*
-    messages.foreachRDD {
-	rdd => rdd.foreach {
-            msg => storeJson(msg._2, sc)
-        }
-    }
-    */
     val rows = messages.map(msg => jsonToRow(msg._2))
-    rows.saveToCassandra("sdtd", "temperatures", SomeColumns("id", "date", "temperature"))
-
-    /*
-    val lines = messages.map(_._2)
-    val words = lines.flatMap(_.split(" "))
-    val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
-    wordCounts.print()
-    */
+    //rows.saveToCassandra("sdtd", "temperatures", SomeColumns("id", "date", "temperature"))
 
     ssc.start()
     ssc.awaitTermination()
   }
 
   def jsonToRow(msg: String) : (String, String, Double) = {
-    val json = JSON.parseFull(msg).get.asInstanceOf[Map[String,Any]]
-    val date = json("date")
-    val temperature = json("temperature")
-    return (com.datastax.driver.core.utils.UUIDs.timeBased().toString(), date.toString, temperature.asInstanceOf[Double])
+    println(JSON.parseFull(msg))
+    //val json = JSON.parseFull(msg).get.asInstanceOf[Map[String,Any]]
+    //val date = json("date")
+    //val temperature = json("temperature")
+    return (com.datastax.driver.core.utils.UUIDs.timeBased().toString(), "ABCDE", 2.0)
+    //return (com.datastax.driver.core.utils.UUIDs.timeBased().toString(), date.toString, temperature.asInstanceOf[Double])
   }
 }
