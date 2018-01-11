38dbecc6ceecaef57981452acad6c0ed1388bffe
Clément Parizot <clement.parizot@grenoble-inp.org>
2018-01-11 17:12:33 +0100

    test scala local

--- client/src/main/scala/spark/Compute.scala
+++ client/src/main/scala/spark/Compute.scala
@@ -27,16 +27,14 @@ object Compute {
     rdd.toString
     *///======================
 
-    /*val conf = new SparkConf().setAppName("test").setMaster("spark://" + GlobalVars.sparkAddress + ":" + GlobalVars.sparkPort) //setMaster("local[2]"); //setMaster("spark://@:port")
+    val conf = new SparkConf().setAppName("test").setMaster("spark://" + GlobalVars.sparkAddress + ":" + GlobalVars.sparkPort) //setMaster("local[2]"); //setMaster("spark://@:port")
     val sc: SparkContext = new SparkContext(conf);
 
     val count = sc.parallelize(1 to NUM_SAMPLES).filter { _ =>
-      val x = math.random
-      val y = math.random
-      x*x + y*y < 1
-    }.count()
+        val x = math.random
+        val y = math.random
+        x*x + y*y < 1
+      }.count()
     println(s"Pi is roughly ${4.0 * count / NUM_SAMPLES}")
-    */
-    "hey hey\n"
   }
 }
