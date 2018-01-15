f28c9e9319b50555fbf1cbb67638f8f531b5bedc
Pablo Cabrera <pablo.cabrera@ensimag.grenoble-inp.fr>
2018-01-15 16:18:07 +0100

    ajout du script d'arrêt des services (arrêt des dockers sur les masters puis sur les slaves avant le shutdown)

--- deployment/stop.py
+++ deployment/stop.py
@@ -1,6 +1,38 @@
+from terminate_instances import terminate_instances
+from get_instances import get_instances, get_public_ip, is_master
+from tools import ssh
+
+
+def stop_master(ip):
+    p = ssh(ip, "sudo docker stop $(sudo docker ps -a -q)")
+    p.wait()
+    print("Master @" + ip + " stopped")
+
+
+def stop_slave(ip):
+    p = ssh(ip, "sudo docker stop $(sudo docker ps -a -q)")
+    p.wait()
+    print("Slave @" + ip + " stopped")
+
+
+def print_header(text):
+    print("\n")
+    print("-------------------")
+    print(text)
+    print("-------------------")
+    print("\n")
+
+
 if __name__ == "__main__":
-    from terminate_instances import terminate_instances
+    print_header("Stoping Masters...")
+    for instance in get_instances(is_slave=False):
+        stop_master(get_public_ip(instance))
 
-    #TODO: stop services with marathon
 
+    print_header("Stoping Slaves...")
+    for instance in get_instances(is_slave=True):
+        stop_slave(get_public_ip(instance))
+
+
+    print_header("Shutting Down...")
     terminate_instances()
