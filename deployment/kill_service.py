6d093810cabe131e40f627b157e43eff92c7eac6
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-01-18 15:38:10 +0100

    Use ip instead of machine id

--- deployment/kill_service.py
+++ deployment/kill_service.py
@@ -1,19 +1,17 @@
-from get_instances import get_instances, get_from_id, is_id_valid, get_public_ip
 from tools import ssh
 
 
-def kill_service(instance, service):
-    p = ssh(get_public_ip(instance), "sudo docker stop " + service)
+def kill_service(pub_ip, service):
+    p = ssh(pub_ip, "sudo docker stop " + service)
     p.wait()
 
 
 if __name__ == "__main__":
     import sys
     try:
-        instance_id = sys.argv[1]
-        assert is_id_valid(instance_id)
+        pub_ip = sys.argv[1]
         service = sys.argv[2]
-    except (AssertionError, IndexError):
-        print("Please specify a valid instance id and a service name")
+    except IndexError:
+        print("Usage: python3 kill_service.py <public-ip> <container-id-or-name>")
         sys.exit(1)
-    kill_service(get_from_id(instance_id), service)
+    kill_service(pub_ip, service)
